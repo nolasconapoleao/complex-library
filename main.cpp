60d012e3a5f637899250cb9fadc82a5e6dec5048
Nolasco Napoleão <nolascoamadonapoleao@gmail.com>
2020-02-17 23:15:09 +0000

    Add use case for complex number in main
    Add construction use case for polar and rectangular complex numbers
    Add print use case of complex number

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+#include "src/complex/PolarComplex.h"
+#include "src/complex/RectangularComplex.h"
+
 int main() {
-  std::cout << "Hello World!";
+    RectangularComplex z1(1, 2);
+    std::cout << z1 << std::endl;
+
+    PolarComplex z2(25, 45);
+    std::cout << z2 << std::endl;
 }
