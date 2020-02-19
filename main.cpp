5bfd0398d77130e91f1ef46a1d61c23f4351411b
Nolasco Napoleão <nolascoamadonapoleao@gmail.com>
2020-02-19 20:22:28 +0000

    Add project Read.md
    Add project description and compilation instructions

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 #include "src/complex/RectangularComplex.h"
 
 int main() {
-    RectangularComplex z1(1, 2);
-    std::cout << z1 << std::endl;
+  RectangularComplex z1(1, 2);
+  std::cout << z1 << std::endl;
 
-    PolarComplex z2(25, 45);
-    std::cout << z2 << std::endl;
+  PolarComplex z2(25, 45);
+  std::cout << z2 << std::endl;
 }
